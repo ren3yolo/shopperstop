d633190bdd5266ca4e1cc29772a78938fd650d4f
Raj Shekhar <channiecx954@gmail.com>
2022-11-27 12:06:13 +0530

    Add products list

---
+++ utils/data.ts
@@ -0,0 +1,81 @@
+export type Product = {
+  name: string;
+  slug: string;
+  category: string;
+  image: string;
+  price: Number;
+  brand: string;
+  rating: number;
+  numReviews: Number;
+  countInStock: Number;
+  description: string;
+};
+
+type Data = {
+  products: Product[];
+};
+
+export const data: Data = {
+  products: [
+    {
+      name: "Free Shirt",
+      slug: "free-shirt",
+      category: "Shirts",
+      image: "/images/shirt1.jpg",
+      price: 70,
+      brand: "Nike",
+      rating: 4.7,
+      numReviews: 23,
+      countInStock: 20,
+      description: "A popular shirt",
+    },
+    {
+      name: "Fit Shirt",
+      slug: "fit-shirt",
+      category: "Shirts",
+      image: "/images/shirt2.jpg",
+      price: 70,
+      brand: "Nike",
+      rating: 4.7,
+      numReviews: 23,
+      countInStock: 20,
+      description: "A popular shirt",
+    },
+    {
+      name: "Slim Shirt",
+      slug: "slim-shirt",
+      category: "Shirts",
+      image: "/images/shirt3.jpg",
+      price: 70,
+      brand: "Raymond",
+      rating: 4.7,
+      numReviews: 23,
+      countInStock: 20,
+      description: "A popular shirt",
+    },
+    {
+      name: "Golf Pants",
+      slug: "golf-pants",
+      category: "Pants",
+      image: "/images/pants1.jpg",
+      price: 70,
+      brand: "Van Heusen",
+      rating: 4.7,
+      numReviews: 23,
+      countInStock: 20,
+      description: "A popular golf pant",
+    },
+    {
+      name: "Chinos",
+      slug: "chinos",
+      category: "Pants",
+      image: "/images/pants2.jpg",
+      price: 70,
+      brand: "Van Heusen",
+      rating: 4.3,
+      numReviews: 23,
+      countInStock: 20,
+      description: "A popular chino",
+    },
+  ],
+};
