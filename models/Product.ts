606c7c06a7f2512a7aca595d8bf4f961090c4940
Raj Shekhar <channiecx954@gmail.com>
2022-12-04 20:03:39 +0530

    Seed sample products

---
+++ models/Product.ts
@@ -0,0 +1,23 @@
+import { Schema, model, models } from "mongoose";
+import { Product as ProductType } from "../utils/data";
+
+const ProductSchema = new Schema<ProductType>(
+  {
+    name: { type: String, required: true },
+    slug: { type: String, unique: true, required: true },
+    category: { type: String, required: true },
+    image: { type: String, required: true },
+    price: { type: Number, required: true },
+    brand: { type: String, required: true },
+    rating: { type: Number, required: true },
+    countInStock: { type: Number, required: true },
+    description: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = models.Product || model<ProductType>("Product", ProductSchema);
+
+export default Product;
--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -1,13 +1,19 @@
 import db from "../../utils/db";
 import User from "../../models/User";
-import { data, User as UserType } from "../../utils/data";
+import Product from "../../models/Product";
+import {
+  data,
+  User as UserType,
+  Product as ProductType,
+} from "../../utils/data";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   await db.connect();
   await User.deleteMany();
-
   await User.insertMany<UserType>(data.users);
+  await Product.deleteMany();
+  await Product.insertMany<ProductType>(data.products);
   await db.disconnect();
   return res.status(200).send({ message: "Seeded successfully" });
 };
